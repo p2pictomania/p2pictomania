681ab8aa2bad29bdeaae5059a0c2d6f6ec001415
Karan Dwivedi <karan1337@live.com>
2016-03-26 04:56:25 -0400

    connections code added

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-	"log"
-
 	"github.com/p2pictomania/p2pictomania/bootstrap"
+	"github.com/p2pictomania/p2pictomania/connections"
 	"github.com/p2pictomania/p2pictomania/web"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
+
+	if len(os.Args) < 3 {
+		log.Println("Usage: ./p2pictomania portno nodename")
+		return
+	}
+
+	tempPort, err := strconv.Atoi(os.Args[1])
+
+	if err != nil {
+		log.Println("Usage: ./p2pictomania portno nodename")
+		return
+	}
+
+	tempName := os.Args[2]
+
+	connections.NodeListenPort = tempPort
+	connections.NodeNickName = tempName
+
 	go web.StartServer()
-	bootstrap.StartServer()
-	log.Println("Server started successfully")
+	go bootstrap.StartServer()
+
+	connections.InitSocketCache(&connections.Sc)
+	go connections.ServerListener(connections.NodeListenPort)
+
+	//block forever
+	select {}
 }
