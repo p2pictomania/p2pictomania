ba57be27870dfccd09b55446ad21baa9fe24910b
Karan Dwivedi <karan1337@live.com>
2016-04-08 03:28:17 -0400

    ttl set to 1

--- bootstrap/server.go
+++ bootstrap/server.go
@@ -66,7 +66,7 @@ func addSelfToDNS() error {
 		log.Fatalf("Cannot get public IP: %s", err)
 		return err
 	}
-	var jsonStr = []byte(`{"record": {"name": "", "record_type": "A", "content": "` + publicIP + `", "ttl": 2, "prio": 10}}`)
+	var jsonStr = []byte(`{"record": {"name": "", "record_type": "A", "content": "` + publicIP + `", "ttl": 1, "prio": 10}}`)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("X-DNSimple-Token", Config.DnsimpleAuthToken)
 	req.Header.Set("Content-Type", "application/json")
