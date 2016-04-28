d1f2c21bb5433dce920ae5e4dc7eb1b16458e66b
Abhishek Shivanna <abhisheks91@gmail.com>
2016-04-28 00:29:23 -0700

    changed number of bootstrap nodes to 4

--- bootstrap/constants.go
+++ bootstrap/constants.go
@@ -4,7 +4,7 @@ const (
 	// BootstrapPort is where the bootstrap server will bind on
 	BootstrapPort int = 5000
 	// MaxNumBootstrapNode is the max number of bootstrap nodes in the network
-	MaxNumBootstrapNode int = 3
+	MaxNumBootstrapNode int = 4
 	//ConfigFileName holds the name of the config file
 	ConfigFileName string = "bootstrap/config.json"
 	// DBFolder holds the path to the database folder
